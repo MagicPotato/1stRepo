87ff5f12b27fa602f454f3ce3ebe055c4c785367
MagicPotato <unigames2013@yandex.ru>
2013-12-26 22:31:12 +0400

    blablabla

---
+++ WebApplication3/Default.aspx
@@ -0,0 +1,95 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication3.Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title></title>
+    <link rel='stylesheet' type="text/css" href="stylesheet.css" />
+</head>
+
+
+<body>
+    <form id="form1" runat="server">
+    <div>
+      <div class="Label">
+      <asp:label ID="Label3" runat="server" Font-Size="3em" text="UpLoad & DownLoad service"></asp:label>
+      </div>
+      <div id="ControlPanel">
+        <div>  
+            <asp:Label ID="Label1" runat="server" Text="Below you can change description of any file."></asp:Label> 
+        </div>
+
+        <div>      
+            <asp:TextBox ID="CDTB" runat="server" TextMode="MultiLine" Rows="5" Width="300"></asp:TextBox>
+        </div>
+
+        <div>
+            <asp:Label ID="Label2" runat="server" Text="Also you can upload you own files."></asp:Label>
+        </div>
+
+        <div>
+            <asp:FileUpload ID="OurFileUpload" runat="server" />
+        </div>
+
+        <div>
+            <asp:TextBox ID="ULTB" runat="server" TextMode="MultiLine" Rows="5" Width="300" Text="description has not been writen yet..."></asp:TextBox>
+        </div>
+
+        <div>
+            <asp:Button ID="ULbutton" runat="server" Text="Upload" 
+                onclick="ULbutton_Click" />
+        </div>
+       </div>
+
+        <asp:Repeater runat="server" ID="Repeater1">
+            <HeaderTemplate>
+                <div id="rptr">
+            </HeaderTemplate>
+            <ItemTemplate>
+            <div class="BLOK">
+                <div class="name">
+                <%# Eval("Name") %>
+                </div>
+
+                <div class="description">
+                <%# Eval("Description") %>
+                </div>
+
+                <div>
+                <asp:Button runat="server" Text="Delete" 
+                        ID="ButtonRemove" 
+                        OnClick="delete_Click" 
+                        CommandArgument='<%# Eval("Id") %>'
+                        UseSubmitBehavior="False" />
+                </div>
+
+                <div>
+                <asp:Button runat="server" Text="Download" 
+                        ID="ButtonDownLoad" 
+                        OnClick="loadButton_Click" 
+                        CommandArgument='<%# Eval("Id") %>'
+                        UseSubmitBehavior="False" />
+                </div>
+
+                <div>
+                <asp:Button runat="server" Text="Change description" 
+                        ID="Button2" 
+                        OnClick="changeButton_Click" 
+                        CommandArgument='<%# Eval("Id") %>'
+                        UseSubmitBehavior="False" />
+                </div>
+
+                <div id="line"></div>
+
+            </div>
+            </ItemTemplate>
+            <FooterTemplate>
+                </div>
+            </FooterTemplate>
+        </asp:Repeater>
+        
+    </div>
+    </form>
+</body> 
+</html>
---
+++ WebApplication3/Default.aspx.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections;
+using System.Data.SqlClient;
+
+namespace WebApplication3
+{
+    public class OurItem
+    {
+        public int id;
+        public string name;
+        public string description;
+
+        public string Name { get {return name;} }
+        public string Description { get {return description;} }
+        public int Id { get { return id; } }
+    }
+
+    public partial class Default : System.Web.UI.Page
+    {
+        List<OurItem> our_items=new List<OurItem>();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string myConnectionString = "Server=tcp:fiiomvs75r.database.windows.net,1433;Database=LabaDB;User ID=laba4305@fiiomvs75r;Password=1qazse432!;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
+            using (SqlConnection connection = new SqlConnection(myConnectionString))
+            {
+                connection.Open();
+                string sqlQuery = "select * from OurTable";
+                SqlCommand command = new SqlCommand(sqlQuery, connection/*, transaction*/);
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var newitem = new OurItem();
+                    newitem.id = reader.GetInt32(0);
+                    newitem.name = reader.GetString(1);
+                    newitem.description = reader.GetString(2);
+                    our_items.Add(newitem);
+                }
+                reader.Close();
+                connection.Close();
+            }
+        }
+
+        void DataBind()
+        {
+            Repeater1.DataSource=our_items;
+            Repeater1.DataBind();
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            DataBind();
+        }
+        protected void delete_Click(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            var id = Convert.ToInt32(button.CommandArgument);
+            int n = -1;
+            do
+            {
+                n++;
+            }
+            while (id != our_items[n].id);
+
+            string myConnectionString = "Server=tcp:fiiomvs75r.database.windows.net,1433;Database=LabaDB;User ID=laba4305@fiiomvs75r;Password=1qazse432!;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
+            using (SqlConnection connection = new SqlConnection(myConnectionString))
+            {
+                connection.Open();
+                string sqlQuery = "DELETE FROM OurTable WHERE Id = " + id + ";";
+                SqlCommand command = new SqlCommand(sqlQuery, connection/*, transaction*/);
+                var reader = command.ExecuteNonQuery();
+                connection.Close();
+                System.IO.File.Delete(Server.MapPath("//") + our_items[n].name);
+            }
+            Response.Redirect("~/Default.aspx");
+        }
+
+        protected void loadButton_Click(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            var id = Convert.ToInt32(button.CommandArgument);
+            int n = -1;
+            do
+            {
+                n++;
+            }
+            while (id != our_items[n].id);
+
+            Page.Response.Redirect("~/" + our_items[n].name);
+        }
+
+        protected void changeButton_Click(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            var id = Convert.ToInt32(button.CommandArgument);
+           
+            string myConnectionString = "Server=tcp:fiiomvs75r.database.windows.net,1433;Database=LabaDB;User ID=laba4305@fiiomvs75r;Password=1qazse432!;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
+            using (SqlConnection connection = new SqlConnection(myConnectionString))
+            {
+                connection.Open();
+                string sqlQuery = "update OurTable set Description='" + CDTB.Text + "' Where ID=" + id + ';';
+                SqlCommand command = new SqlCommand(sqlQuery, connection/*, transaction*/);
+                var reader = command.ExecuteNonQuery();
+                connection.Close();
+            }
+            Page.Response.Redirect("~/Default.aspx");
+        }
+
+        protected void ULbutton_Click(object sender, EventArgs e)
+        {
+            if (OurFileUpload.HasFile)
+            {
+                string path = Server.MapPath("//");
+
+                OurFileUpload.SaveAs(path + OurFileUpload.FileName);
+
+                var OurNewFile = new OurItem();
+                OurNewFile.name = OurFileUpload.FileName;
+                OurNewFile.description = ULTB.Text;
+
+
+                string myConnectionString = "Server=tcp:fiiomvs75r.database.windows.net,1433;Database=LabaDB;User ID=laba4305@fiiomvs75r;Password=1qazse432!;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
+                using (SqlConnection connection = new SqlConnection(myConnectionString))
+                {
+                    connection.Open();
+                    var transaction = connection.BeginTransaction();
+
+                    string sqlQuery = "select ID from OurTable";
+                    SqlCommand command = new SqlCommand(sqlQuery, connection, transaction);
+                    var reader = command.ExecuteReader();
+                    int lastID = 0;
+                    while (reader.Read())
+                    {
+                        lastID = reader.GetInt32(0);
+                    }
+                    reader.Close();
+                    OurNewFile.id = lastID + 1;
+                    sqlQuery = "insert into OurTable (ID, Name, Description)  values (" + OurNewFile.id + "," + "'" + OurNewFile.name + "'" + "," + "'" + OurNewFile.description + "');";
+                    command = new SqlCommand(sqlQuery, connection, transaction);
+                    var reader1 = command.ExecuteNonQuery();
+                    transaction.Commit();
+                    connection.Close();
+                }
+                Page.Response.Redirect("~/Default.aspx");
+            }
+        }
+    }
+}
